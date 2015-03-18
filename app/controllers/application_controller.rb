a69c588e3d6f4464fc377df696a9db1e153db88c
Grace Mekarski and Joey Sandine <jasandine@gmail.com>
2015-03-18 13:08:28 -0700

    Add feature to hide log in and sign up links after user logs in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
   end
-end
 
   def logged_in?
-    !!@current_user
+    if  session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+      @current_user = true
+    end
   end
+end
